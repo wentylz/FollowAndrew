51ed7d4ac5802a681c31015514ca85956b25749a
Wentylz <hitmeneki@gmail.com>
2021-08-09 09:22:21 +0200

    First commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+    wp_footer()
+    ?>
+
+
+<footer class="footer text-center py-2 theme-bg-dark">
+   
+    <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+    <?php
+	dynamic_sidebar('footer-1');
+	?>
+</footer>
+
+</div>
+
+</body>
+</html> 
